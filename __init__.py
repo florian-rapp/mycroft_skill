840fadd2d235b621308ae53e55720bed786bd37d
Rapp Florian <fr061@hdm-stuttgart.de>
2020-12-18 15:12:39 +0100

    fixed handling of empty list

--- __init__.py
+++ __init__.py
@@ -5,46 +5,43 @@ import creds
 
 
 class Nextcalendar(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+	def __init__(self):
+		MycroftSkill.__init__(self)
 
-    @intent_file_handler('nextcalendar.intent')
-    def handle_nextcalendar(self, message):
-        url = f"https://{creds.user}:{creds.pw}@next.social-robot.info/nc/remote.php/dav"
-        today = datetime.combine(datetime.today(), time(0, 0))
+	@intent_file_handler('nextcalendar.intent')
+	def handle_nextcalendar(self, message):
+		url = f"https://{creds.user}:{creds.pw}@next.social-robot.info/nc/remote.php/dav"
 
-        client = caldav.DAVClient(url)
-        principal = client.principal()
-        calendars = principal.calendars()
+		client = caldav.DAVClient(url)
+		principal = client.principal()
+		calendars = principal.calendars()
 
-        appointments = []
-        starts = []
+		appointments = []
+		starts = []
 
-        if len(calendars) > 0:
+		if len(calendars) > 0:
 
-            for calendar in calendars:
-                events = calendar.date_search(today)
-                event = events[0]
-                event.load()
-                e = event.instance.vevent
-                appointments.append(e)
-                starts.append(e.dtstart.value)
+			for calendar in calendars:
+				events = calendar.date_search(datetime.now())
+				if(len(events)>0):
+					event = events[0]
+					event.load()
+					e = event.instance.vevent
+					appointments.append(e)
+					starts.append(e.dtstart.value)
 
-            earliest = min(starts)
-            ind = starts.index(earliest)
-            earliest_appointment = appointments[ind]
+			if(len(starts)==0):
+				self.speak("You haven't got an upcoming appointment")
+			else:
+				earliest = min(starts)
+				earliest_appointment = appointments[starts.index(earliest)]
 
-            try:
-                location = earliest_appointment.location.value
-            except:
-                location = None
-            start = (earliest_appointment.dtstart.value)
-            summary = (earliest_appointment.summary.value)
-            output = f"Your next appointment is on {(start).strftime('%B')} {(start).strftime('%d')}, {(start).strftime('%y')} at {(start).strftime('%H:%m')} and is entitled {summary}."
-            self.speak(output)
-
-        else:
-            self.speak("You haven't got an upcoming appointment")
+				start = (earliest_appointment.dtstart.value)
+				summary = (earliest_appointment.summary.value)
+				output = f"Your next appointment is on {(start).strftime('%B')} {(start).strftime('%d')}, {(start).strftime('%y')} at {(start).strftime('%H:%M')} and is entitled {summary}."
+				self.speak(output)
+		else:
+			self.speak("You haven't got an upcoming appointment")
 
 
 def create_skill():
